ee06926058a4bfc67c950447b339eca342ee1250
BildadM <bildadmuchemi.w@gmail.com>
2023-11-28 08:07:54 -0800

    tebahpla

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(ord('z'), ord('A') - 1, -1):
+    char = chr(i).swapcase()
+    end_char = '' 
+    if i != ord('A') 
+    else ' '
+    print(f"{char}{end_char}", end='')
